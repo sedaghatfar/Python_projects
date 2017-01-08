872f64c2293aad08072b3690d5f77423a5a62f18
Matthew Sedaghatfar <sedaghatfar@gmail.com>
2017-01-08 12:27:08 -0500

    Create sefaria_test.py

---
+++ sefaria_test.py
@@ -0,0 +1,45 @@
+
+import string
+import re
+import requests
+import json
+from PIL import ImageFont
+
+# valid text reference
+sefariaRef="Mishnah_Berakhot.1.1" 
+
+
+#set some more variables.
+fontSize = 10
+fontDrawSize = 18
+font = ImageFont.truetype("MiriamMonoCLM-Book.ttf", fontDrawSize)
+sampleDensity=1
+
+
+def main(sefariaRef):
+    #text input    
+    url = 'http://www.sefaria.org/api/texts/'+sefariaRef
+    params = dict(
+    	commentary=0,
+    	context=0
+    )
+    resp = requests.get(url=url, params=params)
+        
+    data = json.loads(resp.text)
+     
+
+    text = json.dumps(data['he']).decode('unicode-escape')
+    eng = json.dumps(data['text']).decode('unicode-escape')    
+    text = text.replace(u"\u05BE"," ")
+    exclude = set(string.punctuation+u"\uFEFF"+"\n")
+    text = ''.join(char for char in text if char not in exclude)
+    strip_cantillation_vowel_regex = re.compile(ur"[^\u05d0-\u05f4\s]", re.UNICODE)
+    text = strip_cantillation_vowel_regex.sub('', text)
+    
+    print '\n'    
+    print text
+    print '\n'
+    print eng
+
+if __name__ == '__main__':	
+	main("Mishnah_Berakhot.1.1")
