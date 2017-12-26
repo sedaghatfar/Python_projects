a48cc5b3b11f367b9bdeb84eb0c8206c4de9728b
Matthew Sedaghatfar <sedaghatfar@gmail.com>
2017-12-26 08:11:05 -0500

    Update crypto_check.py

--- crypto_check.py
+++ crypto_check.py
@@ -22,17 +22,14 @@ OMG = getTokenPrice("OmiseGO")
 ETH = getTokenPrice("Ethereum")
 LTC = getTokenPrice("Litecoin")
 ENG = getTokenPrice("enigma-project")
-REQ = getTokenPrice("request-network")
 
-#Enter HODLings
-
-current = (ARK * 1 + BTC * 1 + OMG * 1 + ETH * 1 + LTC * 1 + ENG * 1) 
+current = (ARK * 104.37 + BTC * 0.02 + OMG * 55.28 + ETH * 6 + LTC + ENG * 94.9)
 print datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p")
-print "Ark : " + str(ARK)
+print "ARK : " + str(ARK)
 print "BTC : " + str(BTC)
-print "OMG : " + str(OMG)
+print "ENG : " + str(ENG)
 print "ETH : " + str(ETH)
 print "LTC : " + str(LTC)
-print "ENG : " + str(ENG)
+print "OMG : " + str(OMG)
 
 print "\nCurrent value is ${:,.2f}\n".format(current)
